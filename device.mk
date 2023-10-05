eb4bd6304d7c86f305cfe8052dd70bc9271a8f37
AmeChanRain <downloadbot007@gmail.com>
2023-10-05 12:26:45 +0000

    pdx215: Ship prebuilt libprotobuf-cpp
    * libprotobuf-cpp-full-vendorcompat & libprotobuf-cpp-lite-vendorcompat are deprecated

--- device.mk
+++ device.mk
@@ -248,6 +248,13 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat
 
+# Protobuf Prebuilts
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+
 # QTI Components
 TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
