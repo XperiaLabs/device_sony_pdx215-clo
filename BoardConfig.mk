4f69d2f378c78b0685373c77d8e271c2d5828b46
saku-bruh <saku-bruh@proton.me>
2024-08-05 21:46:29 +0000

    pdx215: Fix errors and conflicts
    * Also update prop-files to align with vendor
    
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,6 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Bypass Errors
+ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
+
 # Inherit Mainline Common BoardConfig
 include build/make/target/board/BoardConfigMainlineCommon.mk
 
