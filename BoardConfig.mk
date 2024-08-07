d086e2bd8f72ad5ddefb939176c23759c4dbd75e
Saku <saku-bruh@proton.me>
2024-08-07 09:32:35 +0300

    pdx215: Import initial SEPolicy
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -318,8 +318,8 @@ BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SEPolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
-include hardware/sony/sepolicy/qti/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/privatess
 
 # SELinux Neverallows
 ifdef SELINUX_IGNORE_NEVERALLOWS
