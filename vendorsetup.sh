b2278b4fcf0a22df0791ae9f8b1d0bd107598ec0
saku-bruh <saku-bruh@proton.me>
2024-08-12 22:36:45 +0300

    pdx215: Update patches & fixes
    * Update aospa.dependencies
    
    * Remove FM folder that causes build to fail
    
    * Add more logic to vendorsetup.sh for fixes
    
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "This script will apply patches to vendor/aospa."
+echo "This script will apply patches and fixes to your ROMs source code."
 echo "Choose an option:"
-echo "1. Apply first build patches and continue (For a first time build or if you have repo synced to update the ROM)"
-echo "2. Skip patching process and continue build (After the first build, if patches have been already been applied)"
+echo "1. Apply first build patches and start building (For a first time build or if you have repo synced to update the ROM)"
+echo "2. Skip patching process and start building (After the first build, if patches have been already been applied)"
 read -p "Enter your choice: " choice
 
 case $choice in
@@ -21,9 +21,22 @@ case $choice in
         echo -e
         echo 'Done Applying all Patches!'
         echo -e
+        echo 'Checking if problematic FM folder exists...'
+        echo -e
+        if [[ -d vendor/qcom/opensource/commonsys/fm ]]; then
+            echo 'Problematic FM folder found! Removing...'
+            rm -rf vendor/qcom/opensource/commonsys/fm/
+            echo 'Problematic FM successfully removed!'
+            echo -e
+        else
+            echo 'Problematic FM folder not found, skipping removal.'
+            echo -e
+        fi
+        echo 'All patches and fixes have been applied! Starting build process...'
+        echo -e
         ;;
     2)
-        echo "Skipping patch application..."
+        echo "Patches and fixes skipped... Starting build process."
         ;;
     *)
         echo "Invalid choice! Using option 2 as a fallback."
@@ -32,5 +45,5 @@ case $choice in
 esac
 
 if [[ "$choice" == "2" ]]; then
-    echo "Skipping patch application..."
+    echo "Patches and fixes skipped... Starting build process."
 fi
