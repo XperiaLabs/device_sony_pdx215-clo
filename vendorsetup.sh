1b787945a74ed2a2021ea0aa7aa5b173fe0bfc40
saku-bruh <saku-bruh@proton.me>
2024-08-12 16:33:14 +0300

    pdx215: Improve patching script logic
    * Now gives you a choice between patching or skipping said patching and doesn't force reset the git history of the repos
    
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-echo -e
-echo 'Applying patches...'
-echo -e
-echo 'Applying patches to vendor/aospa'
-echo -e
-cd vendor/aospa
-git reset --hard FETCH_HEAD
-echo -e
-git am ../../device/sony/pdx215/configs/patches/vendor_aospa/0001-products-Introduce-Sony-Xperia-1-III-pdx215.patch
-cd ../..
-echo -e
-echo 'Done Applying all Patches!'
-echo -e
+echo "This script will apply patches to vendor/aospa."
+echo "Choose an option:"
+echo "1. Apply first build patches and continue (For a first time build or if you have repo synced to update the ROM)"
+echo "2. Skip patching process and continue build (After the first build, if patches have been already been applied)"
+read -p "Enter your choice: " choice
+
+case $choice in
+    1)
+        echo -e
+        echo 'Applying patches...'
+        echo -e
+        echo 'Applying patches to vendor/aospa'
+        echo -e
+        cd vendor/aospa
+        git reset --hard FETCH_HEAD
+        echo -e
+        git am ../../device/sony/pdx215/configs/patches/vendor_aospa/0001-products-Introduce-Sony-Xperia-1-III-pdx215.patch
+        cd ../..
+        echo -e
+        echo 'Done Applying all Patches!'
+        echo -e
+        ;;
+    2)
+        echo "Skipping patch application..."
+        ;;
+    *)
+        echo "Invalid choice! Using option 2 as a fallback."
+        choice=2
+        ;;
+esac
+
+if [[ "$choice" == "2" ]]; then
+    echo "Skipping patch application..."
+fi
